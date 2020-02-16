3d0f8c9c7f6a5ca99b0ae32bc2b7ff6c65f270d6
Hitesh_Humad <Hitesh_Humad@PUNITP143483L.ad.infosys.com>
2020-02-16 22:52:42 +0530

    first commit

---
+++ JenkinsDemo/src/main/java/Calculater.java
@@ -0,0 +1,17 @@
+
+public class Calculater {
+	
+
+		public int addVal(int a, int b)
+		{
+		int sum = a+b;		
+		return sum;
+		};
+		
+		public boolean getbool(){
+			
+			return true;
+		}
+			
+	}
+
---
+++ JenkinsDemo/src/test/java/DemoTest.java
@@ -0,0 +1,16 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DemoTest{
+	
+	@Test
+	public void shouldreturnTrue() {
+		
+		Calculater cal = new Calculater();
+		assertTrue(cal.getbool());	
+			
+	}
+	
+}
